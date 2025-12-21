6fa271d55909e0881f26a9cf6824b006b044714e
techphenom <techphenom@gmail.com>
2025-12-21 14:56:30 -0500

    Reusing bytebuffers that are direct allocated. App crashes when turning off server with connected device

--- app/src/main/java/com/techphenom/usbipserver/server/AttachedDeviceContext.kt
+++ app/src/main/java/com/techphenom/usbipserver/server/AttachedDeviceContext.kt
@@ -6,11 +6,13 @@ import android.hardware.usb.UsbEndpoint
 import android.util.SparseArray
 import com.techphenom.usbipserver.server.protocol.ongoing.UsbIpBasicPacket
 import com.techphenom.usbipserver.server.protocol.ongoing.UsbIpSubmitUrb
+import com.techphenom.usbipserver.server.protocol.utils.Logger
 import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.sync.Semaphore
 import java.net.Socket
 import java.nio.ByteBuffer
 import java.util.concurrent.ConcurrentHashMap
+import java.util.concurrent.ConcurrentLinkedQueue
 
 class AttachedDeviceContext {
     lateinit var device: UsbDevice
@@ -21,9 +23,29 @@ class AttachedDeviceContext {
     val transferSemaphore = Semaphore(permits = MAX_CONCURRENT_TRANSFERS)
     var totalEndpointCount: Int = 0
     val replyChannel = Channel<UsbIpBasicPacket>(Channel.UNLIMITED)
+    private val bufferPool = ConcurrentLinkedQueue<ByteBuffer>()
 
     companion object {
         const val MAX_CONCURRENT_TRANSFERS = 50
+    }
+
+    fun acquireBuffer(size: Int): ByteBuffer {
+        val iterator = bufferPool.iterator()
+        while (iterator.hasNext()) {
+            val buf = iterator.next()
+            if (buf.capacity() >= size) {
+                iterator.remove()
+                buf.clear()
+                Logger.i("AttachedDevCon", "Reusing Buffer, capacity: ${buf.capacity()}, position: ${buf.position()}, limit: ${buf.limit()}")
+                return buf
+            }
+        }
+        return ByteBuffer.allocateDirect(maxOf(size, 16384))
+    }
+
+    fun releaseBuffer(buffer: ByteBuffer?) {
+        buffer?.clear()
+        if(buffer != null) bufferPool.offer(buffer)
     }
 
     data class PendingTransfer(
